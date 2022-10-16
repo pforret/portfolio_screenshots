3e17999c4df16bdde453e4cece9b4a5ce1587b23
Bashew Runner <actions@users.noreply.github.com>
2022-10-16 17:24:22 +0200

    MOD: screenshots.2022-10-16.log screenshots.sh

--- screenshots.sh
+++ screenshots.sh
@@ -133,9 +133,12 @@ Script:main() {
     git add -A
     timestamp=$(date -u)
     message="$timestamp < $script_basename $script_version < ${os_name=-} ${os_version:-}"
+    IO:log "Finishing GitHub Action: $message"
+    # don't modify any file after the next line, so also no log file
     git commit -m "${message}" || exit 0
     git pull --rebase
     git push
+    exit 0
 
     ;;
 
